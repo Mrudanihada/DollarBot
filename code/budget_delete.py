9447dd490ba222562100a842100757ea59024835
Jk42github <160773137+Jk42github@users.noreply.github.com>
2024-10-24 01:42:16 +0000

    updated budget_delete.py

--- code/budget_delete.py
+++ code/budget_delete.py
@@ -24,15 +24,24 @@ SOFTWARE.
 
 """
 
+import logging
 import helper
-
+from telebot import types
 
 def run(message, bot):
     chat_id = message.chat.id
     user_list = helper.read_json()
-    print(user_list)
-    if str(chat_id) in user_list:
-        user_list[str(chat_id)]['budget']['overall'] = None
-        user_list[str(chat_id)]['budget']['category'] = None
+
+    if str(chat_id) not in user_list:
+        bot.send_message(chat_id, "You don't have budget data to delete.")
+    else:
+        if "budget" in user_list[str(chat_id)]:
+            # The 'budget' dictionary exists; you can proceed with deleting it
+            user_list[str(chat_id)]["budget"] = {"overall": None, "category": {}}
+        else:
+            bot.send_message(chat_id, "No budget data to delete.")
+                
         helper.write_json(user_list)
-    bot.send_message(chat_id, 'Budget deleted!')
+        bot.send_message(chat_id, "Budget data deleted successfully.")
+
+    helper.write_json(user_list)
