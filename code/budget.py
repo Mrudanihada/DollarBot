828ac5a72ee0401f932608736735ab2b067b14e5
Jk42github <160773137+Jk42github@users.noreply.github.com>
2024-10-24 01:39:52 +0000

    updated budget.py

--- code/budget.py
+++ code/budget.py
@@ -25,12 +25,11 @@ SOFTWARE.
 """
 
 import helper
+from telebot import types
+import logging
 import budget_view
 import budget_update
 import budget_delete
-import logging
-from telebot import types
-
 
 def run(message, bot):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
@@ -38,24 +37,32 @@ def run(message, bot):
     markup.row_width = 2
     for c in options.values():
         markup.add(c)
-    msg = bot.reply_to(message, 'Select Operation', reply_markup=markup)
+    msg = bot.reply_to(message, "Select Operation", reply_markup=markup)
     bot.register_next_step_handler(msg, post_operation_selection, bot)
-
 
 def post_operation_selection(message, bot):
     try:
         chat_id = message.chat.id
+        user_list = helper.read_json()
         op = message.text
         options = helper.getBudgetOptions()
+        
         if op not in options.values():
-            bot.send_message(chat_id, 'Invalid', reply_markup=types.ReplyKeyboardRemove())
-            raise Exception("Sorry I don't recognise this operation \"{}\"!".format(op))
-        if op == options['update']:
+            bot.send_message(chat_id, "Invalid", reply_markup=types.ReplyKeyboardRemove())
+            raise Exception('Sorry I don\'t recognise this operation "{}"!'.format(op))
+        if str(chat_id) not in user_list:
+            # Initialize the user's data with an empty budget dictionary
+            user_list[str(chat_id)] = helper.createNewUserRecord(message)
+        if op == options["add"]:
             budget_update.run(message, bot)
-        elif op == options['view']:
+        if op == options["update"]:
+            budget_update.run(message, bot)
+        elif op == options["view"]:
             budget_view.run(message, bot)
-        elif op == options['delete']:
+        elif op == options["delete"]:
             budget_delete.run(message, bot)
+    
+        helper.write_json(user_list)
+
     except Exception as e:
-        # print("hit exception")
         helper.throw_exception(e, message, bot, logging)
