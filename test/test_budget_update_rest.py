a9cdbe6214a27ce9cfe6021658549b03b35b714d
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-10-24 19:37:08 +0000

    add test case

--- test/test_budget_update_rest.py
+++ test/test_budget_update_rest.py
@@ -3,19 +3,6 @@ from mock import ANY
 from mock.mock import patch
 from telebot import types
 
-
-
-@patch("telebot.telebot")
-def test_post_category_add(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
-
-    message = create_message("hello from testing!")
-    budget_update.post_category_add(message, mc)
-
-    mc.reply_to.assert_called_with(message, "Select Option", reply_markup=ANY)
-
-
 def create_message(text):
     params = {"messagebody": text}
     chat = types.User(11, False, "test")
